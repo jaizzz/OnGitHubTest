2518afd5809659268ad78566c9e6dcf0aab8b2fb
jazz <jaswant.raj45@gmail.com>
2020-02-19 18:29:12 +0530

    Modified MainActivity added new Main2ctivity

--- app/src/main/java/com/jazzzzz/ongithubtest/Main2Activity.java
+++ app/src/main/java/com/jazzzzz/ongithubtest/Main2Activity.java
@@ -10,5 +10,8 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        System.out.println("hello  Main2Activity");
+
     }
 }
