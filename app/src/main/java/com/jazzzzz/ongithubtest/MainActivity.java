938c4ee9a88cf95ed5beabe8a5af18bc465fdc43
jazz <jaswant.raj45@gmail.com>
2020-02-21 13:18:40 +0530

    commit on 21-feb-2020

--- app/src/main/java/com/jazzzzz/ongithubtest/MainActivity.java
+++ app/src/main/java/com/jazzzzz/ongithubtest/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         System.out.println("hello git");
+        System.out.println("updated on 21-feb-2020");
 
     }
 }
